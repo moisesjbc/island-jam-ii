79b4fe5124ab36100b7542577d9a6d5c11b52931
moisesjbc <moisesjbc@gmail.com>
2017-12-02 13:05:10 +0000

    Added car stuff scene

---
+++ island-jam-ii/Assets/Scripts/StuffGenerator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StuffGenerator : MonoBehaviour {
+
+	public GameObject stuffPrefab;
+	public GameObject leftSpawnPoint;
+	public GameObject rightSpawnPoint;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown (KeyCode.Space)){
+			float x = Random.Range (leftSpawnPoint.transform.position.x, rightSpawnPoint.transform.position.x);
+			Vector3 spawnPoint = new Vector3 (x, leftSpawnPoint.transform.position.y, 0.0f);
+			
+			float angle = Random.Range (0.0f, 360.0f);
+			Debug.Log (angle);
+			Quaternion spawnRotation = Quaternion.AngleAxis (angle, Vector3.forward);
+			Instantiate (stuffPrefab, spawnPoint, spawnRotation);
+		}
+	}
+}
