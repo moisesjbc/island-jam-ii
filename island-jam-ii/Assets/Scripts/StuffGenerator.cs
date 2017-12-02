5a39144f306b50c0de445c4f7e1fdf4e96c08169
moisesjbc <moisesjbc@gmail.com>
2017-12-02 16:35:04 +0000

    Added shaking without destroying

--- island-jam-ii/Assets/Scripts/StuffGenerator.cs
+++ island-jam-ii/Assets/Scripts/StuffGenerator.cs
@@ -11,6 +11,12 @@ public class StuffGenerator : MonoBehaviour {
 
 	public List<GameObject> stuffObjects = new List<GameObject>();
 
+	public float shakeForceMinX = 3.0f;
+	public float shakeForceMaxX = 7.0f;
+	public float shakeForceMinY = 3.0f;
+	public float shakeForceMaxY = 7.0f;
+
+
 	// Use this for initialization
 	void Start () {
 
@@ -18,10 +24,12 @@ public class StuffGenerator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown (KeyCode.Space)){
+		if (Input.GetKeyDown (KeyCode.Space)) {
 			AddObject ();
-		}else if(Input.GetKeyDown (KeyCode.S)){
+		} else if (Input.GetKeyDown (KeyCode.D)) {
 			Shake ();
+		}else if(Input.GetKeyDown (KeyCode.S)){
+			Shake (false);
 		}else if(Input.GetKeyDown (KeyCode.C)){
 			Clear ();
 		}
@@ -41,7 +49,7 @@ public class StuffGenerator : MonoBehaviour {
 		stuffObjects.Add (newStuff);
 	}
 
-	void Shake(int nStuff = 4) {
+	void Shake(bool remove=true, int nStuff = 4) {
 		// Check that there is something to remove.
 		if (nStuff > 0) {
 			// If we try to remove more stuff than we have we 
@@ -53,9 +61,17 @@ public class StuffGenerator : MonoBehaviour {
 			for (int i=0; i<nStuff; i++) {
 				int removeElementIndex = Random.Range (0, stuffObjects.Count);
 				Debug.Log (stuffObjects [0].transform.position);
-				Destroy (stuffObjects [removeElementIndex].GetComponent <BoxCollider2D> ());
-				stuffObjects [removeElementIndex].transform.Translate (Vector3.forward);
-				stuffObjects.RemoveAt (removeElementIndex);
+
+				//stuffObjects [removeElementIndex].transform.Translate (Vector3.forward);
+				Vector2 force = new Vector2(Random.Range (shakeForceMinX, shakeForceMaxX), Random.Range (shakeForceMinY, shakeForceMaxY));
+
+				if(stuffObjects [removeElementIndex].GetComponent<Rigidbody2D>() != null){
+					stuffObjects [removeElementIndex].GetComponent<Rigidbody2D>().AddForce (force, ForceMode2D.Impulse);
+					if (remove) {
+						Destroy (stuffObjects [removeElementIndex].GetComponent <BoxCollider2D> ());
+						stuffObjects.RemoveAt (removeElementIndex);
+					}
+				}
 			}
 		}
 	}
